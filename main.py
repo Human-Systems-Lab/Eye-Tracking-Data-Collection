fd14bdb621cec24276f6bf96baa88e4a2f962d7d
dmorle <morled@uwindsor.ca>
2020-10-08 23:56:20 -0400

    started skeleton

--- main.py
+++ main.py
@@ -1,8 +1,82 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
 import pygame
+
+pAP = None
+background = (0, 0, 0)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Panel(ABC):
+    def __init__(self, rect: pygame.Rect):
+        """
+        :param rect: Panel rectangle in (left, top, right, bottom) format
+        """
+        self.rect = rect
+
+    @abstractmethod
+    def __contains__(self, p: Point):
+        """
+        Checks if the given point is within the panel
+
+        :param p: point to check
+        :return: boolean result
+        """
+        if (
+                self.rect.left < p.x < self.rect.right and
+                self.rect.top < p.y < self.rect.bottom
+        ):
+            return True
+        return False
+
+    @abstractmethod
+    def display(self) -> None:
+        pass
+
+    @abstractmethod
+    def l_down(self) -> None:
+        pass
+
+    @abstractmethod
+    def l_up(self) -> None:
+        pass
+
+    @abstractmethod
+    def l_release(self) -> None:
+        pass
+
+
+def onPaint(screen, panels: List[Panel]):
+    screen.fill(background)
+
+    p = pygame.mouse.get_pos()
+    for e in panels:
+        if p in e:
+            e.display()
 
 
 def main():
-    pass
+    screen = pygame.display.set_mode(flags=pygame.RESIZABLE)
+    pygame.display.set_caption("HMS : Eye Tracking Data Collection")
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.VIDEORESIZE:
+                screen.fill(background)
+                screen = pygame.display.set_mode(event.size, flags=pygame.RESIZABLE)
+
+        onPaint(screen, [])
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
---
+++ serialization.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import boto3
+
+
+class Serializer(ABC):
+    @abstractmethod
+    def __enter__(self):
+        pass
+
+    @abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print(f'exc_type: {exc_type}')
+            print(f'exc_value: {exc_val}')
+            print(f'exc_traceback: {exc_tb}')
+
+    @abstractmethod
+    def handle_data(self, point: Tuple[float], frame) -> None:
+        """
+        Serializes a frame of data to the selected location
+
+        :param point: the location on the screen where the person was prompted to look
+        :param frame: picture of person looking at point
+        """
+
+
+class DiskSerializer(Serializer):
+    def __init__(self, path):
+        pass
+
+    def __enter__(self):
+        raise NotImplementedError()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+    def handle_data(self, point: Tuple[float], frame) -> None:
+        raise NotImplementedError()
+
+
+class S3Serializer(Serializer):
+    def __init__(self, bucket, aws_access_key_id=None, aws_secret_access_key=None):
+        self.client = boto3.client(
+            service_name="s3",
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
+        self.bucket = bucket
+
+    def __enter__(self):
+        raise NotImplementedError()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+    def handle_data(self, point: Tuple[float], frame) -> None:
+        raise NotImplementedError()
