950a2ba1592fc145dcb174f82c5abc1eaf36bb07
dmorle <morled@uwindsor.ca>
2020-11-26 21:19:07 -0500

    overhaul of eye prompter widget to accomidate prompt configs

--- Prompter.py
+++ Prompter.py
@@ -1,6 +1,7 @@
 import time
 import random
 import threading
+from typing import Optional
 
 import cv2
 
@@ -14,160 +15,138 @@ from PyQt5.QtGui import QKeyEvent
 from Serialization import Serializer
 
 
-class ResourceLock:
-    """
-    wrapper on threading.Lock to enable use alongside the with block
-    """
-
-    def __init__(self):
-        self._lock = threading.Lock()
-
-    def __enter__(self):
-        try:
-            self._lock.acquire()
-        except Exception as e:
-            self._lock.release()
-            raise e
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self._lock.release()
-        if exc_type:
-            print(f'exc_type: {exc_type}')
-            print(f'exc_value: {exc_value}')
-            print(f'exc_traceback: {exc_traceback}')
-
-    def locked(self):
-        self._lock.locked()
-
-    def acquire(self, *args, **kwargs):
-        self._lock.acquire(*args, **kwargs)
-
-    def release(self):
-        self._lock.release()
-
-
 class EyePrompt(QWidget):
-    def __init__(self, *args, **kwargs):
-        super(EyePrompt, self).__init__(*args, **kwargs)
-        self.setCursor(Qt.BlankCursor)
+    def __init__(self, config, serializer: Serializer):
+        super(EyePrompt, self).__init__()
 
-        self._runningPrompts = False
-        self._runningPromptsLock = ResourceLock()
-        self._serializer = None
-        self._serializerLock = ResourceLock()
-        self._dataThread = None
-        self._dataThreadLock = ResourceLock()
-        self._cycleLength = 1
-        self._cycleLengthLock = ResourceLock()
+        self.prompt_size = config["prompt_size"]
+        self.corner_prompt_idx = 4 if config["corner_prompts"] else 0
+        self.export_metadata = config["metadata"]
+        self.serializer = serializer
 
-        self._startTime = None
-        self._prompt_loc = None
+        self.start_time = None
+        self.prompt_loc_lock = threading.Lock()
+        self.prompt_loc = None
 
-        self.blackBrush = QtGui.QBrush()
-        self.blackBrush.setColor(QtGui.QColor("black"))
-        self.blackBrush.setStyle(Qt.SolidPattern)
+        self.black_brush = QtGui.QBrush()
+        self.black_brush.setColor(QtGui.QColor("black"))
+        self.black_brush.setStyle(Qt.SolidPattern)
 
-        self.redBrush = QtGui.QBrush()
-        self.redBrush.setColor(QtGui.QColor("red"))
-        self.redBrush.setStyle(Qt.SolidPattern)
+        self.prompt_brush = QtGui.QBrush()
+        self.prompt_brush.setColor(QtGui.QColor(*config["prompt_color"]))
+        self.prompt_brush.setStyle(Qt.SolidPattern)
 
-    @property
-    def runningPrompts(self) -> bool:
-        with self._runningPromptsLock:
-            return self._runningPrompts
+    def start_prompts(self):
+        raise NotImplementedError("EyePrompt.start_prompts is an abstract method")
 
-    @runningPrompts.setter
-    def runningPrompts(self, val: bool) -> None:
-        with self._runningPromptsLock:
-            self._runningPrompts = val
-
-    @property
-    def serializer(self) -> Serializer:
-        with self._serializerLock:
-            return self._serializer
-
-    @serializer.setter
-    def serializer(self, val: Serializer) -> None:
-        with self._serializerLock:
-            self._serializer = val
-
-    @property
-    def dataThread(self) -> threading.Thread:
-        with self._dataThreadLock:
-            return self._dataThread
-
-    @dataThread.setter
-    def dataThread(self, val: threading.Thread) -> None:
-        with self._dataThreadLock:
-            self._dataThread = val
-
-    @property
-    def cycleLength(self) -> float:
-        with self._cycleLengthLock:
-            return self._cycleLength
-
-    @cycleLength.setter
-    def cycleLength(self, val: float) -> None:
-        with self._cycleLengthLock:
-            self._cycleLength = val
-
-    def startPrompts(self):
-        self._startTime = time.time()
-
-        th = threading.Thread(target=self.collectData)
-        th.start()
-        self.dataThread = th
-
-    def endPrompts(self):
-        self.runningPrompts = False
-        self.dataThread.join()
-
-    def collectData(self):
-        cycleNum = 1
-        self._prompt_loc = (
-            random.uniform(0, 1),
-            random.uniform(0, 1)
-        )
-        cap = cv2.VideoCapture(0)
-
-        self.runningPrompts = True
-        while self.runningPrompts:
-            # assumption is that the time it take to run this loop is much less than self.cycleLength
-            if time.time() > self._startTime + cycleNum * self.cycleLength:
-                cycleNum += 1
-                self._prompt_loc = (
-                    random.uniform(0, 1),
-                    random.uniform(0, 1)
-                )
-                self.update()
-
-            ret, frame = cap.read()
-            if not ret:
-                continue
-
-            if self.serializer is not None:
-                self.serializer.handle_data(self._prompt_loc, frame)
-
-        cap.release()
+    def shutdown(self):
+        raise NotImplementedError("EyePrompt.shutdown is an abstract method")
 
     def paintEvent(self, e: QtGui.QPaintEvent) -> None:
         painter = QtGui.QPainter(self)
 
         size = (painter.device().width(), painter.device().height())
         rect = QtCore.QRect(0, 0, *size)
-        painter.fillRect(rect, self.blackBrush)
+        painter.fillRect(rect, self.black_brush)
 
-        dim = 10
         painter.setBrush(self.redBrush)
-        if self.runningPrompts:
-            x = int(self._prompt_loc[0] * size[0])
-            y = int(self._prompt_loc[1] * size[1])
-            painter.drawEllipse(x, y, dim, dim)
-        else:
-            pass
+        with self.prompt_loc_lock:
+            if self.prompt_loc is not None:
+                painter.drawEllipse(
+                    int(self.prompt_loc[0] * size[0]),
+                    int(self.prompt_loc[1] * size[1]),
+                    self.prompt_size,
+                    self.prompt_size
+                )
 
     def keyPressEvent(self, e: QKeyEvent) -> None:
         k = e.key()
         if k == Qt.Key_Escape:
             self.endPrompts()
             self.close()
+
+
+class MousePrompt(EyePrompt):
+    def __init__(self, config):
+        super(MousePrompt, self).__init__(config)
+        self.setCursor(Qt.ArrowCursor)
+
+        self.trigger_space = 10
+        self.cap = cv2.VideoCapture(0)
+
+    def start_prompts(self):
+        self.start_time = time.time()
+        if self.corner_prompt_idx != 0:
+            self.prompt_loc = (0, 0)
+        else:
+            self.prompt_loc = (
+                random.uniform(0, 1),
+                random.uniform(0, 1)
+            )
+
+    def shutdown(self):
+        self.cap.release()
+        # TODO: more shutdown stuff, idk what
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        if self.prompt_loc is None:
+            return super().mousePressEvent(a0)
+
+        m_geo = self.geometry()
+        x_loc = int(self.prompt_loc[0] * m_geo.width())
+        y_loc = int(self.prompt_loc[1] * m_geo.height())
+
+        if (
+                a0.button() == Qt.LeftButton and
+                x_loc - self.trigger_space < a0.x() < x_loc + self.trigger_space and
+                y_loc - self.trigger_space < a0.y() < y_loc + self.trigger_space
+        ):
+            # TODO: Capture image
+            if self.corner_prompt_idx != 0:
+                if self.corner_prompt_idx == 3:
+                    self.prompt_loc = (0, 1)
+                elif self.corner_prompt_idx == 2:
+                    self.prompt_loc = (1, 1)
+                else:
+                    self.prompt_loc = (1, 0)
+                self.corner_prompt_idx -= 1
+            else:
+                self.prompt_loc = (
+                    random.uniform(0, 1),
+                    random.uniform(0, 1)
+                )
+                self.update()
+        else:
+            return super().mousePressEvent(a0)
+
+
+class TimerPrompt(EyePrompt):
+    def __init__(self, config):
+        super(TimerPrompt, self).__init__(config)
+        self.setCursor(Qt.BlankCursor)
+
+        self.prompt_loc_th = None
+        self.capture_th = None
+
+    def start_prompts(self):
+        self.prompt_loc_th = threading.Thread(target=self.run_prompt_loc)
+        self.capture_th = threading.Thread(target=self.run_capture)
+
+    def run_prompt_loc(self):
+        pass
+
+    def run_capture(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+
+def create_prompter(config) -> Optional[EyePrompt]:
+    if config["trigger"] == "Mouse":
+        return MousePrompt(config)
+
+    if config["trigger"] == "Timer":
+        return TimerPrompt(config)
+
+    return None
